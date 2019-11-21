a5cd59a8098b217b4b3c4b0b73263cb6e05bddc3
Samuel Gustin <gustin.daniel1@gmail.com>
2019-11-22 01:07:03 +0200

    Added Index route
    Index view
    Enabled auto migrations

---
+++ Wikipedia-Clone/Wikipedia-Clone/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wikipedia_Clone.Models;
+
+namespace Wikipedia_Clone.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Article
+        public ActionResult Index()
+        {
+            var articles = db.Articles;
+            ViewBag.Articles = articles;
+            return View();
+        }
+    }
+}
