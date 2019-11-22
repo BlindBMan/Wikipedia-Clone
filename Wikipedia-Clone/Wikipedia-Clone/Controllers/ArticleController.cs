f9db4095aee1e19b808649a759210183069f20d7
Samuel Gustin <gustin.daniel1@gmail.com>
2019-11-22 14:29:23 +0200

    CRUD on Articles

--- Wikipedia-Clone/Wikipedia-Clone/Controllers/ArticleController.cs
+++ Wikipedia-Clone/Wikipedia-Clone/Controllers/ArticleController.cs
@@ -11,12 +11,93 @@ namespace Wikipedia_Clone.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Article
+        [HttpGet]
         public ActionResult Index()
         {
             var articles = db.Articles;
             ViewBag.Articles = articles;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult New()
+        {
+            Article article = new Article();
+            return View(article);
+        }
+
+        [HttpPost]
+        public ActionResult New(Article article)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Articles.Add(article);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(article);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(article);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Show(int id)
+        {
+            Article article = db.Articles.Find(id);
+            return View(article);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Article article = db.Articles.Find(id);
+            return View(article);
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Article reqArticle)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Article article = db.Articles.Find(id);
+
+                    if (TryUpdateModel(article))
+                    {
+                        article.Content = reqArticle.Content;
+                        article.Title = reqArticle.Title;
+                        article.Date = reqArticle.Date;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(reqArticle);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(reqArticle);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
